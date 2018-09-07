3113020dc73ae9b1b01d923a34c793daf6d42d72
lbl <libi_lang@163.com>
2018-09-07 10:21:23 +0800

    test

---
+++ hellolibrary/src/main/java/com/blbase/www/hellolibrary/CustomUtil.java
@@ -0,0 +1,14 @@
+package com.blbase.www.hellolibrary;
+
+/**
+ * author：libilang
+ * time: 2018/9/7 09:42
+ * 邮箱：libi_lang@163.com
+ */
+
+public class CustomUtil {
+
+    public static String getMyHello(){
+        return "icodeyou";
+    }
+}
---
+++ hellolibrary/src/main/java/com/blbase/www/hellolibrary/MainActivity.java
@@ -0,0 +1,18 @@
+package com.blbase.www.hellolibrary;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author：libilang
+ * time: 2018/9/7 09:40
+ * 邮箱：libi_lang@163.com
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
